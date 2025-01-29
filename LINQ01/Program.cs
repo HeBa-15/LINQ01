0d3e5e86a66da2effa31e8b2d466d782777b7e08
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-30 00:54:46 +0200

    What Is LINQ

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -4,6 +4,19 @@
     {
         static void Main(string[] args)
         {
+            #region LINQ
+
+            //LINQ => Language Integrated Query , C# 3.0
+            // +40 Extension Method Exists in Enumerable class categorized under 13
+            //can be used with any type implements IEnumerable interface => {List ,Array ,Dictionary,....}
+
+            // Sequence => Collection
+            // 1. Local Sequence  => in memory object , Files{L2O,L2XML,...} 
+            // 2. Remote Sequence => L2EF {Sql, Mongo Db ,mysql,Oracle}
+
+
+
+            #endregion
 
         }
     }
