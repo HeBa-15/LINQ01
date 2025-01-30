c77236450ec722750d085695e9dfffe726050bde
HeBa-15 <hebamahmoudali1510@gmail.com>
2025-01-30 02:40:35 +0200

    LINQ Syntax

--- LINQ01/Program.cs
+++ LINQ01/Program.cs
@@ -18,6 +18,48 @@
 
             #endregion
 
+            #region LINQ Syntax
+
+            List<int> numbers = new List<int> {1,2,3,4,5,6,7,8,9 };
+
+            // 1. Fluent Syntax
+
+            // 1.1 Calling static method
+
+            // var result = Enumerable.Where(numbers, n => n % 2 == 0);
+
+            // 1.2 Extension Method 
+
+            // var result = numbers.Where(n => n%2 == 0 && n > 5);
+
+
+            //*****************************************
+
+            // 2.Query Syntax (Query Expression)
+
+            //staring with "From" => "Range variable" in "Collection" must End with select or Group by 
+
+            //var result = from n in numbers
+            //             where n % 2 == 0 && n > 5
+            //             select n;
+
+            //*******************************************
+
+            // 3.hyprid Syntax
+
+            // var result = (from n in numbers
+            //             where n % 2 == 0 
+            //             select n).Where(n => n > 5);
+
+
+
+            //foreach (int n in result)
+            //{
+            //    Console.Write($"{n},");
+            //}
+
+            #endregion
+
         }
     }
 }
